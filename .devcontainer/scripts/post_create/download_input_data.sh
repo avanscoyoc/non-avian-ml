e586eb3f97ef57997ef6676e1fbacb018e3a37d6
Amy Van Scoyoc <amy.e.van.scoyoc@gmail.com>
2025-04-01 22:16:14 +0000

    added kfold scripts and updated download_input_data from toy to full dataset

--- .devcontainer/scripts/post_create/download_input_data.sh
+++ .devcontainer/scripts/post_create/download_input_data.sh
@@ -12,16 +12,16 @@ if [ ! -d "/workspaces/non-avian-ml-toy" ]; then
 fi
 
 # Download data if not already present
-if [ ! -f "/workspaces/non-avian-ml-toy/data.zip" ]; then
+if [ ! -f "/workspaces/non-avian-ml-toy/non-avian_ML.zip" ]; then
     echo "Downloading input data..."
-    curl -L -o /workspaces/non-avian-ml-toy/data.zip "https://storage.googleapis.com/dse-staff-public/data.zip"
+    curl -L -o /workspaces/non-avian-ml-toy/non-avian_ML.zip "https://storage.googleapis.com/dse-staff-public/non-avian_ML.zip"
 fi
 
 # Verify download before unzipping
-if [ -f "/workspaces/non-avian-ml-toy/data.zip" ]; then
+if [ -f "/workspaces/non-avian-ml-toy/non-avian_ML.zip" ]; then
     echo "Unzipping data..."
-    unzip -o /workspaces/non-avian-ml-toy/data.zip -d /workspaces/non-avian-ml-toy/
-    rm /workspaces/non-avian-ml-toy/data.zip
+    unzip -o /workspaces/non-avian-ml-toy/non-avian_ML.zip -d /workspaces/non-avian-ml-toy/
+    rm /workspaces/non-avian-ml-toy/non-avian_ML.zip
     echo "Data extraction complete!"
 else
     echo "Download failed. File not found!"
