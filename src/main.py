4b345bebc18fab53e6992c3d48cd766bd9a9b570
Amy Van Scoyoc <amy.e.van.scoyoc@gmail.com>
2025-04-09 23:04:22 +0000

    updates to argparser and k8; still need args. in results of main.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,55 @@
 from utils import *
+import argparse
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Run BirdNET model training and evaluation."
+    )
+    parser.add_argument(
+        "--datapath", type=str, required=True, help="Path to the audio data directory."
+    )
+    parser.add_argument(
+        "--species_list",
+        type=str,
+        nargs="+",
+        required=True,
+        help="List of species to include in the training.",
+    )
+    parser.add_argument(
+        "--datatype",
+        type=str,
+        choices=["data", "data_5s"],
+        required=True,
+        help="Type of data to process (3 or 5 seconds).",
+    )
+    parser.add_argument(
+        "--model_name", type=str, required=True, help="Name of the model to use."
+    )
+    parser.add_argument(
+        "--batch_size", type=int, default=4, help="Batch size for training."
+    )
+    parser.add_argument(
+        "--folds", type=int, default=2, help="Number of folds for cross-validation."
+    )
+    parser.add_argument(
+        "--training_size",
+        type=int,
+        default=25,
+        help="Number of samples to use for training.",
+    )
+    parser.add_argument(
+        "--random_seed", type=int, default=2, help="Random seed for reproducibility."
+    )
+    parser.add_argument(
+        "--results_path",
+        type=str,
+        default="results",
+        help="Path to save the results.",
+    )
+    args = parser.parse_args()
+
     results = main(
-        datapath="/workspaces/non-avian-ml-toy/data/audio/",
+        datapath=args.datapath,
         species_list=["human_vocal"],
         datatype="data",
         model_name="BirdNET",
