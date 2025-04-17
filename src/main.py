f1f13e75d128dd36a83070ca23f77dcbfdafa08c
Amy Van Scoyoc <amy.e.van.scoyoc@gmail.com>
2025-04-17 20:23:24 +0000

    updates to argparse

--- src/main.py
+++ src/main.py
@@ -1,63 +1,17 @@
 from utils import *
-import argparse
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Run BirdNET model training and evaluation."
-    )
-    parser.add_argument(
-        "--datapath", type=str, required=True, help="Path to the audio data directory."
-    )
-    parser.add_argument(
-        "--species_list",
-        type=str,
-        nargs="+",
-        required=True,
-        help="List of species to include in the training.",
-    )
-    parser.add_argument(
-        "--datatype",
-        type=str,
-        choices=["data", "data_5s"],
-        required=True,
-        help="Type of data to process (3 or 5 seconds).",
-    )
-    parser.add_argument(
-        "--model_name", type=str, required=True, help="Name of the model to use."
-    )
-    parser.add_argument(
-        "--batch_size", type=int, default=4, help="Batch size for training."
-    )
-    parser.add_argument(
-        "--folds", type=int, default=2, help="Number of folds for cross-validation."
-    )
-    parser.add_argument(
-        "--training_size",
-        type=int,
-        default=25,
-        help="Number of samples to use for training.",
-    )
-    parser.add_argument(
-        "--random_seed", type=int, default=2, help="Random seed for reproducibility."
-    )
-    parser.add_argument(
-        "--results_path",
-        type=str,
-        default="results",
-        help="Path to save the results.",
-    )
     args = parser.parse_args()
-
-    results = main(
+    results = run_model(
         datapath=args.datapath,
-        species_list=["human_vocal"],
-        datatype="data",
-        model_name="BirdNET",
-        batch_size=4,
-        folds=2,
-        training_size=75,
-        random_seed=2,
-        results_path="/workspaces/non-avian-ml-toy/results",
+        species_list=args.species_list,
+        datatype=args.datatype,
+        model_name=args.model_name,
+        batch_size=args.batch_size,
+        folds=args.folds,
+        training_size=args.training_size,
+        random_seed=args.random_seed,
+        results_path=args.results_path,
     )
 
     if results is None:
--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import roc_auc_score
 import random
 from datetime import datetime
-
+import argparse
 
 class DataLoader:
     """Handles loading and preprocessing of data."""
@@ -217,7 +217,52 @@ class ResultManager:
         return filepath
 
 
-def main(
+parser = argparse.ArgumentParser(
+    description="Run BirdNET model training and evaluation."
+)
+parser.add_argument(
+    "--datapath", type=str, default="/workspaces/non-avian-ml-toy/data/audio", help="Path to the audio data directory."
+)
+parser.add_argument(
+    "--species_list",
+    type=str,
+    nargs="+",
+    required=True,
+    help="List of species to include in the training.",
+)
+parser.add_argument(
+    "--datatype",
+    type=str,
+    choices=["data", "data_5s"],
+    required=True,
+    help="Type of data to process (3 or 5 seconds).",
+)
+parser.add_argument(
+    "--model_name", type=str, default="BirdNET", help="Name of the model to use."
+)
+parser.add_argument(
+    "--batch_size", type=int, default=4, help="Batch size for training."
+)
+parser.add_argument(
+    "--folds", type=int, default=2, help="Number of folds for cross-validation."
+)
+parser.add_argument(
+    "--training_size",
+    type=int,
+    default=10,
+    help="Number of samples to use for training.",
+)
+parser.add_argument(
+    "--random_seed", type=int, default=1, help="Random seed for reproducibility."
+)
+parser.add_argument(
+    "--results_path",
+    type=str,
+    default="/workspaces/non-avian-ml-toy/results",
+    help="Path to save the results.",
+)
+
+def run_model(
     datapath,
     species_list,
     datatype,
@@ -265,3 +310,4 @@ def main(
         )
 
     return results
+
